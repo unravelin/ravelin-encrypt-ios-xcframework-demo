573afdb22c8ce4b9bc1d4f816c2dccd42f25d532
Mark Brightman <mark.brightman@ravelin.com>
2022-01-14 09:44:40 +0000

    update date used for encrypt example

--- Shared/UseRavelin.swift
+++ Shared/UseRavelin.swift
@@ -42,7 +42,7 @@ struct UseRavelin {
     func useEncrypt() {
         // Encrypt customer card details, ready for sending for payment
         var error:NSError? = nil
-        let encryptionPayload = encrypt.encrypt("41111111111111", month: "10", year: "21", nameOnCard: "Mr John Doe", error: &error)
+        let encryptionPayload = encrypt.encrypt("41111111111111", month: "10", year: "23", nameOnCard: "Mr John Doe", error: &error)
         if let error = error {
             print("Ravelin encryption error \(error.localizedDescription)")
         } else {
